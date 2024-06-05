b27890b48023ad998583ef28efe5d0b2501c0b8b
vinay-chindam <vinnuhary@gmail.com>
2024-06-05 10:13:30 +0530

    object cloing

--- index.js
+++ index.js
@@ -33,3 +33,25 @@ console.log(rect1.draw())
 
 let rect2=new Rectangle(20,30)
 console.log(rect2.bre)
+
+let obj2={
+    name:"abdfds",
+    city:"hyd",
+    age:14,
+
+}
+let obj3={}
+
+for(let key in obj2){
+    obj3[key]=obj2[key]
+    
+}
+console.log(obj3)
+
+let dest=Object.assign({},obj3)
+console.log(dest)
+let dest2={...dest}
+console.log(dest2)
+
+// object cloning
+
